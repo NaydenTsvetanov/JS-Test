9517b5dafbff7d0dff8496829feb43f1a0b02988
Naka <n.tsvetanov99@gmail.com>
2022-06-24 15:02:58 +0300

    first_display_working

--- display.js
+++ display.js
@@ -1,6 +1,12 @@
-for(var i = 0; i < 100; i++){
-    for(var j = 0; j < 150; j++){
-        console.log("#");
+let element = document.getElementById("myDiv");
+
+for(var i = 0; i < 25; i++){
+    for(var j = 0; j < 100; j++){
+        if(i == 0 || j == 0 || i == 24 || j == 99){
+            element.innerHTML += "#";
+        }else {
+            element.innerHTML += " . ";
+        }
     }
-    console.log("#");
+    element.innerHTML += "<br>";
 }
